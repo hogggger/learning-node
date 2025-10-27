d9fa4b2d4345ebb026bc9a50c68359a5693cc9cd
hogger <deuntlt@vip.qq.com>
2025-10-27 21:44:10 +0800

    feat: 新增index.js

--- index.js
+++ index.js
@@ -1,2 +1,65 @@
 // 新增入口文件
-// ceshi 
+const querystring = require('querystring');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const game = require('./game');
+
+let playerWon = 0;
+
+let playerLastAction = null;
+let sameCount = 0;
+
+http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url,'http://localhost:8080');
+
+    if(parsedUrl.pathname === '/favicon.ico'){
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+    if(parsedUrl.pathname === '/game'){
+        const query = querystring.parse(parsedUrl.query);
+        const playerAction = query.action;
+
+        if(playerLastAction && playerLastAction === playerAction){
+            sameCount++;
+        } else {
+            sameCount = 0;
+        }
+
+        if(sameCount >= 3){
+            res.writeHead(500);
+            res.end('你作弊！连续三次出一样的，游戏结束！');
+            return;
+        }
+
+        const gameResult = game(playerAction);
+
+        if(gameResult === 1){
+            playerWon++;
+        } else if(gameResult === -1){
+            playerWon = 0;
+        }
+
+        if(playerWon >= 3){
+            res.writeHead(500);
+            res.end('你太厉害了，连续赢了三次，游戏结束！');
+            return;
+        }
+
+        playerLastAction = playerAction;
+
+        res.writeHead(200);
+        res.end(`你出的是${playerAction}, 结果是${gameResult === 0 ? '平局' : gameResult === 1 ? '你赢了' : '你输了'}`);
+    }
+
+    // 处理根路径，返回HTML页面
+    if(parsedUrl.pathname === '/'){
+        fs.createReadStream('./index.html').pipe(res);
+    }
+}).listen(8080, () => {
+    console.log('服务器已启动，访问地址：http://localhost:8080');
+}
+
